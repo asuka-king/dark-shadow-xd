38fada5b43ee00b789dc66e72471cd8327e54fa5
asuka-king <group.asukaking@gmail.com>
2025-03-28 20:47:34 +0530

    Add files via upload
    Signed-off-by: asuka-king <group.asukaking@gmail.com>

---
+++ config.js
@@ -0,0 +1,102 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const defaultOwner = '94769484004';
+
+
+// Check for the OWNERS environment variable; if not found, use the default
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '94769484004' ; // put your number here
+
+const ownerlist = ownervb.split(';');
+
+global.owner = [];
+for (let i = 0; i < ownerlist.length; i++) {
+    global.owner.push([ownerlist[i], true]);
+}
+//
+global.botname = process.env.BOTNAME || 'ᴅᴀʀᴋ ꜱʜᴀᴅᴏᴡ-xᴅ';
+global.pairingNumber = process.env.BOT_NUMBER || '94769484004' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '5wAyFJZT#9nzgGTPH73lDCoeQ9yNgOl0QOq33oVvBLCesLX0isZM' ;  // put your session id here
+
+global.mods = []
+global.prems = []
+global.allowed = ['94769484004', '94769484004']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.canal = 'https://whatsapp.com/channel/0029Vb9cu0FIiRoxxrIMee3J'
+
+
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz',
+}
+global.APIKeys = {
+  // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api.fgmods.xyz': 'm2XBbNvz',
+}
+
+// Sticker WM
+global.premium = 'true'
+global.packname = 'ᴅᴀʀᴋ ꜱʜᴀᴅᴏᴡ-xᴅ'
+global.author = '✦ᴀꜱᴜᴋᴀ ᴋɪɴɢ✦'
+global.menuvid = 'https://imgur.com/a/kIlaoq1.mp4'
+global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/asukaking'
+global.dygp = 'https://whatsapp.com/channel/0029Vb9cu0FIiRoxxrIMee3J'
+global.fgsc = 'https://github.com/asuka-king/dark-shadow-xd'
+global.fgyt = 'https://youtube.com/@asuka-king-sl'
+global.fgpyp = 'https://github.com/asuka-king'
+global.fglog = 'https://files.catbox.moe/yod3dj.jpg'
+global.thumb = fs.readFileSync('./assets/jin.jpg')
+
+global.wait = '⏳'
+global.rwait = '⏳'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌'
+global.xmoji = '🤩'
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
